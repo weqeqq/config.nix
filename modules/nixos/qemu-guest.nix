b2321413ce15675edbaf17527cda11f827500bc9
Kononov Ivan <ggmodwear@gmail.com>
2026-03-15 08:09:59 +0300

    fix: include virtio storage modules for vm test host

--- modules/nixos/qemu-guest.nix
+++ modules/nixos/qemu-guest.nix
@@ -3,6 +3,17 @@ let
   qemuGuestEnabled = (hostVars.virtualization or { }).qemuGuest or false;
 in
 lib.mkIf qemuGuestEnabled {
+  boot.initrd.availableKernelModules = [
+    "ata_piix"
+    "sd_mod"
+    "sr_mod"
+    "virtio_blk"
+    "virtio_mmio"
+    "virtio_net"
+    "virtio_pci"
+    "virtio_scsi"
+  ];
+
   services.qemuGuest.enable = true;
   services.spice-vdagentd.enable = true;
 
